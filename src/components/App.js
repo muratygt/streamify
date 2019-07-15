49cdf5bde9ee982cfde4ad678224b531630cba58
Murat <muratyigit88@gmail.com>
2019-07-15 16:48:32 -0400

    Setting up Redux store

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { Router, Route } from 'react-router-dom';
 import StreamEdit from '../components/streams/StreamEdit';
 import StreamShow from '../components/streams/StreamShow';
 import StreamDelete from '../components/streams/StreamDelete';
 import StreamCreate from '../components/streams/StreamCreate';
 import StreamList from '../components/streams/StreamList';
 import Header from './Header';
+import history from '../history';
 
 const App = () => {
 	return (
 		<div className="ui container">
-			<BrowserRouter>
+			<Router history={history}>
 				<div>
 					<Header />
 					<Route path="/" exact component={StreamList} />
-					<Route path="/streams/new" component={StreamCreate} />
-					<Route path="/streams/edit" component={StreamEdit} />
-					<Route path="/streams/delete" component={StreamDelete} />
-					<Route path="/streams/show" component={StreamShow} />
+					<Route path="/streams/new" exact component={StreamCreate} />
+					<Route path="/streams/edit/:id" exact component={StreamEdit} />
+					<Route path="/streams/delete" exact component={StreamDelete} />
+					<Route path="/streams/show" exact component={StreamShow} />
 				</div>
-			</BrowserRouter>
+			</Router>
 		</div>
 	);
 };
